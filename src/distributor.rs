d61d08e923ad0c6ce15180b882f5bc8b045d9c53
Dirk Gadsden <dirk@esherido.com>
2016-02-18 19:07:00 -0800

    Organize distributor into separate file

---
+++ src/distributor.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+use metrics::*;
+
+pub struct Distributor {
+    counts: HashMap<String, Count>,
+    measures: HashMap<String, Measure>,
+
+    // TODO: Implement samples
+    // samples: HashMap<String, Sample>,
+
+    /// How often it will collect its metrics before reporting the accumulated
+    /// value of the metric.
+    default_resolution: Seconds,
+}
+
+impl Distributor {
+    pub fn new() -> Distributor {
+        Distributor {
+            counts: HashMap::new(),
+            measures: HashMap::new(),
+            samples: HashMap::new(),
+            default_resolution: 10,
+        }
+    }
+
+    pub fn record_measure(&mut self, name: String, value: f64) {
+        let resolution = self.default_resolution;
+
+        let measure = self.measures.entry(name.clone()).or_insert_with(|| {
+            Measure {
+                metric: Metric::new(name, resolution),
+                values: vec![],
+            }
+        });
+
+        measure.values.push(value)
+    }
+
+    pub fn record_count(&mut self, name: String, value: u64) {
+        let resolution = self.default_resolution;
+
+        let entry = self.counts.entry(name.clone()).or_insert_with(|| {
+            Count {
+                metric: Metric::new(name, resolution),
+                value: 0,
+            }
+        });
+
+        entry.value = value
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,46 +3,11 @@ extern crate lazy_static;
 
 extern crate regex;
 
+mod distributor;
 mod reader;
 mod metrics;
 
-use std::collections::HashMap;
-use metrics::*;
-
-struct Distributor {
-    counts: HashMap<String, Count>,
-    measures: HashMap<String, Measure>,
-    sample: HashMap<String, Sample>,
-    default_resolution: Seconds,
-}
-
-impl Distributor {
-    pub fn record_measure(&mut self, name: String, value: f64) {
-        let resolution = self.default_resolution;
-
-        let measure = self.measures.entry(name.clone()).or_insert_with(|| {
-            Measure {
-                metric: Metric::new(name, resolution),
-                values: vec![],
-            }
-        });
-
-        measure.values.push(value)
-    }
-
-    pub fn record_count(&mut self, name: String, value: u64) {
-        let resolution = self.default_resolution;
-
-        let entry = self.counts.entry(name.clone()).or_insert_with(|| {
-            Count {
-                metric: Metric::new(name, resolution),
-                value: 0,
-            }
-        });
-
-        entry.value = value
-    }
-}
+pub use distributor::Distributor;
 
 #[cfg(test)]
 mod test {
