bef937bfbf393d604d281826101235c732268399
Dirk Gadsden <dirk@esherido.com>
2016-02-20 21:50:52 -0800

    Start building StatsD TCP server

--- src/collectors/mod.rs
+++ src/collectors/mod.rs
@@ -1,2 +1,4 @@
 /// Tools for building collectors to be exposed through the Iron HTTP library.
 pub mod http;
+
+pub mod statsd;
---
+++ src/collectors/statsd.rs
@@ -0,0 +1,42 @@
+use std::io::{BufRead, BufReader};
+use std::net::TcpListener;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+
+pub struct StatsdListener {
+    listener: TcpListener,
+}
+
+impl StatsdListener {
+    pub fn listen(&self) {
+        let (send, recv) = channel();
+
+        let listener = self.listener.try_clone().unwrap();
+        thread::spawn(move || {
+            accept_on_listener(listener, send)
+        });
+
+        for line in recv {
+            self.handle_line(line)
+        }
+    }
+
+    fn handle_line(&self, line: String) {
+
+    }
+}
+
+fn accept_on_listener(listener: TcpListener, send: Sender<String>) {
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let mut reader = BufReader::new(stream);
+                let mut line = String::new();
+
+                reader.read_line(&mut line);
+                send.send(line).unwrap();
+            },
+            Err(e) => panic!("Failed to listen on TCP socket: {}", e),
+        }
+    }
+}
