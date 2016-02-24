c409dcccaa64b19c6643e9b62a41bb46861c869c
Dirk Gadsden <dirk@esherido.com>
2016-02-23 18:54:25 -0800

    Full parsing of StatsD messages

--- src/parsers/statsd.rs
+++ src/parsers/statsd.rs
@@ -1,4 +1,6 @@
 use std::char;
+use std::error;
+use std::fmt;
 use std::num::ParseIntError;
 use std::str::{self, FromStr};
 
@@ -20,17 +22,54 @@ pub enum ParsedMetric {
 
 pub type ParseResult<'a> = IResult<&'a [u8], ParsedMetric>;
 
+// Convert a byte array to a `u64` result.
 fn bytes_to_u64(i: &[u8]) -> Result<u64, ParseIntError> {
     let s = str::from_utf8(i).unwrap();
 
     u64::from_str(s)
 }
 
+#[derive(Debug, PartialEq)]
+pub struct ParseError {
+    description: String,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description)
+    }
+}
+
+impl error::Error for ParseError {
+    fn description(&self) -> &str {
+        &self.description
+    }
+}
+
+pub fn parse_metrics(i: &[u8]) -> Result<Vec<ParsedMetric>, ParseError> {
+    let result = complete!(i,
+        separated_nonempty_list!(
+            tag!("\n"),
+            alt_complete!(
+                parse_counter |
+                parse_gauge |
+                parse_timer
+            )
+        )
+    );
+
+    match result {
+        IResult::Done(_, metrics) => Ok(metrics),
+        IResult::Error(err) => Err(ParseError { description: format!("{:?}", err) }),
+        IResult::Incomplete(_) => unreachable!("Cannot be Incomplete"),
+    }
+}
+
 pub fn parse_counter(i: &[u8]) -> ParseResult {
     chain!(i,
         name: parse_metric_name ~ tag!(":")  ~
         value: parse_value      ~ tag!("|c") ~
-        sample_rate: opt!(complete!(parse_sample_rate)) ,
+        _sample_rate: opt!(complete!(parse_sample_rate)) ,
 
         ||{ ParsedMetric::Counter(name, value) }
     )
@@ -123,6 +162,28 @@ mod tests {
         assert_eq!(
             parse_timer(&b"foo.bar_baz:12|ms"[..]),
             complete(ParsedMetric::Timer("foo.bar_baz".to_owned(), 12))
+        )
+    }
+
+    #[test]
+    fn it_parse_single_metric() {
+        assert_eq!(
+            parse_metrics(&b"foo:1|g"[..]),
+            Ok(vec![
+                ParsedMetric::Gauge("foo".to_owned(), 1),
+            ])
+        )
+    }
+
+    #[test]
+    fn it_parse_many_metrics() {
+        assert_eq!(
+            parse_metrics(&b"foo:1|g\nbar:2|c|@3\nbaz:4|ms"[..]),
+            Ok(vec![
+                ParsedMetric::Gauge("foo".to_owned(), 1),
+                ParsedMetric::Counter("bar".to_owned(), 2),
+                ParsedMetric::Timer("baz".to_owned(), 4),
+            ])
         )
     }
 }
