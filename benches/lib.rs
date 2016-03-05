9b7a686060dd8e2485b0192db31177fd9c15059c
Dirk Gadsden <dirk@esherido.com>
2016-03-04 21:51:19 -0800

    Add basic Heroku log line parser benchmark

---
+++ benches/lib.rs
@@ -0,0 +1,22 @@
+#![feature(test)]
+
+extern crate metrics_distributor;
+extern crate test;
+
+use metrics_distributor::parsers::log_line::*;
+use test::Bencher;
+
+#[bench]
+fn bench_heroku_log_line_reader(b: &mut Bencher) {
+    let lines = vec![
+        "2016-02-26 21:34:59.429615+00:00 heroku web.2 - - source=web.2 dyno=heroku.123.XYZ sample#load_avg_1m=0.56 sample#load_avg_5m=0.26 sample#load_avg_15m=0.17\n",
+        "2016-02-26 21:50:36.352129+00:00 heroku router - - sock=backend at=error code=H18 desc=\"Server Request Interrupted\" method=GET path=\"/\" host=www.example.com request_id=XYZ fwd=\"1.2.3.4\" dyno=web.5 connect=0ms service=495ms status=503 bytes=1648\n",
+        "2016-02-26 21:34:59.370813+00:00 heroku router - - at=info method=PUT path=\"/\" host=www.example.com request_id=XYZ fwd=\"1.2.3.4\" dyno=web.1 connect=0ms service=39ms status=200 bytes=1627\n",
+    ];
+
+    b.iter(|| {
+        lines.iter().map(|line| {
+            HerokuLogLineReader.read(line)
+        }).collect::<Vec<_>>()
+    })
+}
